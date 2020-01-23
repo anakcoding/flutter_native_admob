282ea469b9aa80a938e1977ed43653a7c71a0dfe
anakcoding <m.hafizh.bayhaqi@gmail.com>
2020-01-23 20:35:30 +0700

    typo TRANSPARENT

--- android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdmobBannerViewFactory.kt
+++ android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdmobBannerViewFactory.kt
@@ -234,7 +234,7 @@ class NativeAdmobBannerViewHoder @JvmOverloads constructor(
     val bgColor: Int
     when (style) {
       BannerStyle.dark -> {
-        setBackgroundColor(Color.TRANSPARANT)
+        setBackgroundColor(Color.TRANSPARENT)
 
         adHeadline.setTextColor(Color.WHITE)
         adAdvertiser.setTextColor(Color.WHITE)
@@ -243,7 +243,7 @@ class NativeAdmobBannerViewHoder @JvmOverloads constructor(
         adPrice.setTextColor(Color.WHITE)
       }
       BannerStyle.light -> {
-        setBackgroundColor(Color.TRANSPARANT)
+        setBackgroundColor(Color.TRANSPARENT)
 
         adHeadline.setTextColor(Color.BLACK)
         adAdvertiser.setTextColor(Color.BLACK)
